0248962cb0747dff0ed6d525d3e603fc323c63b5
Moses Fawole <mosesfawole2001@gmail.com>
2023-01-08 19:37:08 +0100

    set states in stateContext.js

---
+++ src/context/stateContext.js
@@ -0,0 +1,10 @@
+import { createContext, useContext, useState } from "react";
+const Context = createContext();
+
+export const StateContext = ({ children }) => {
+  const [showCart, setShowCart] = useState(false);
+  const [cartItems, setCartItems] = useState([]);
+  const [totalPrice, setTotalPrice] = useState(0);
+  const [totalQuantities, setTotalQuantities] = useState(0);
+  const [qty, setQty] = useState(1);
+};
