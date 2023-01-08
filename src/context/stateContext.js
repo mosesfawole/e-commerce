269de4ae83041b390a7e4b93995e01baac06c91b
Moses Fawole <mosesfawole2001@gmail.com>
2023-01-08 19:43:11 +0100

    add to carts functionality

--- src/context/stateContext.js
+++ src/context/stateContext.js
@@ -7,4 +7,27 @@ export const StateContext = ({ children }) => {
   const [totalPrice, setTotalPrice] = useState(0);
   const [totalQuantities, setTotalQuantities] = useState(0);
   const [qty, setQty] = useState(1);
+  let foundProduct;
+  let index;
+
+  const onAdd = (product, quantity) => {
+    const checkProductInCart = cartItems.find((item) => item.id === product.id);
+  };
+  setTotalPrice((prevTotalPrice) => prevTotalPrice + product.price * quantity);
+  setTotalQuantities((prevTotalQuantities) => prevTotalQuantities + quantity);
+
+  if (checkProductInCart) {
+    const updatedCartItems = cartItems.map((cartProduct) => {
+      if (cartProduct.id === product.id)
+        return {
+          ...cartProduct,
+          quantity: cartProduct.quantity + quantity,
+        };
+    });
+    setCartItems(updatedCartItems);
+  } else {
+    product.quantity = quantity;
+
+    setCartItems([...cartItems, { ...product }]);
+  }
 };
