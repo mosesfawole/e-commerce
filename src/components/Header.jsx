e1df99ceee63ae339f3737494a5535a0de4f7ecf
Moses Fawole <mosesfawole2001@gmail.com>
2023-03-20 07:49:56 +0100

    fix mobile menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -56,7 +56,17 @@ const Header = () => {
               <div className="overlay fixed right-0 top-0 w-full z-30 h-full bg-[#00000091]"></div>
               <div className="bg-white fixed left-0 top-0 p-6 z-30 w-[70%] h-full">
                 <div className="" onClick={closeMenu}>
-                  <img src={CloseIcon} alt="close-icon" />
+                  <svg
+                    width="14"
+                    height="15"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="m11.596.782 2.122 2.122L9.12 7.499l4.597 4.597-2.122 2.122L7 9.62l-4.595 4.597-2.122-2.122L4.878 7.5.282 2.904 2.404.782l4.595 4.596L11.596.782Z"
+                      fill="#000"
+                      fill-rule="evenodd"
+                    />
+                  </svg>
                 </div>
                 <ul className="font-bold mt-10 flex flex-col gap-4  text-md">
                   <li>Collections</li>
